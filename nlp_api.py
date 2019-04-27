e07cb3171cb9684cf9ce600849ff333c1133cd6b
Sugamsm <sugamsmankad@gmaill.com>
2019-04-27 17:18:30 -0400

    Initial commit

---
+++ nlp_api.py
@@ -0,0 +1,18 @@
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def first():
+    return '<h1>YOLO</h1>'
+
+
+@app.route('/some', methods=['POST'])
+def getData():
+    print(request.args)
+
+
+app.run()
